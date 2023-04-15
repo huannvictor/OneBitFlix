4075ef9ccaf8e0dbc79e76ab0253141196d01bc5
Huann Almeida <huannvictor@gmail.com>
2023-04-15 18:15:24 -0300

    login com json web token - jwt

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { userService } from "../services/userService";
+import { jwtService } from "../services/jwtService";
 
 export const authController = {
+  //POST /auth/register
   register: async (req: Request, res: Response) => {
     const { firstName, lastName, phone, birth, email, password } = req.body;
 
@@ -27,6 +29,37 @@ export const authController = {
       if (error instanceof Error) {
         return res.status(400).json({ message: error.message });
       }
+    }
+  },
+
+  //POST /auth/login
+  login: async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await userService.findByEmail(email);
+      if (!user)
+        return res.status(404).json({ message: "e-mail não registrado." });
+
+      user.checkPassword(password, (err, isSame) => {
+        if (err) return res.status(400).json({ message: err.message });
+
+        if (!isSame)
+          return res.status(401).json({ message: "senha incorreta" });
+
+        const payload = {
+          id: user.id,
+          firstName: user.firstName,
+          email: user.email,
+        };
+
+        const token = jwtService.signToken(payload, "7d");
+
+        return res.json({ authenticated: true, ...payload, token });
+      });
+    } catch (error) {
+      if (error instanceof Error)
+        return res.status(400).json({ message: error.message });
     }
   },
 };
---
+++ src/services/jwtService.ts
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+
+const secret = "chave-do-jwt"; // essa chave será trocada no futuro. //!É uma falha de segurança deixar assim.
+
+export const jwtService = {
+  signToken: (payload: string | object | Buffer, expiration: string) => {
+    return jwt.sign(payload, secret, {
+      expiresIn: expiration,
+    });
+  },
+};
